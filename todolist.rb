d539ccc48fb0bef924c3df2de4d533a563c3950b
Tianqing <442034859@qq.com>
2016-06-05 15:11:14 +0800

    first version

--- todolist.rb
+++ todolist.rb
@@ -5,12 +5,35 @@ class TodoList
       @items = Array.new
     end
 
-    attr_reader :title, :items
+    attr_reader :items
+    attr_accessor :title  #naming, renaming a list
 
-    def add_item(item_description)
+    def delete_item (id)
+      @items.delete(id-1) #delete item
+    end
+
+    def add_item(item_description)  # add item
       item = Item.new(item_description)
       @items.push(item)
     end
+
+    def update_status(id, true_or_false) #update completion status
+      @items[id-1].change_complete_status (true_or_false)
+    end
+
+    def completed?(id)
+      @items[id-1].@completed_status
+    end
+
+    def print_list
+      @title.length.times {puts "-"}
+      puts @title
+      @title.length.times {puts "-"}
+      counter = 0
+      while counter < @items.length
+        puts "#{counter+1} - #{@items[counter]}" + "\t" + "Completed: #{@items[counter].@completed_status}"
+        counter += 1
+      end
 end
 
 class Item
@@ -19,5 +42,9 @@ class Item
     def initialize(item_description)
       @description = item_description
       @completed_status = false
+    end
+
+    def change_complete_status(true_or_false)
+      @completion = true_or_false
     end
 end
--- udacitask.rb
+++ udacitask.rb
@@ -1,23 +1,24 @@
 require_relative 'todolist.rb'
 
 # Creates a new todo list
-
+mylist = TodoList.new("Pengtianqing's daily routine")
 # Add four new items
-
+4.times.mylist.add_item(gets.chomp)
 # Print the list
-
+mylist.print_list
 # Delete the first item
-
+mylist.delete_item(1)
 # Print the list
-
+mylist.print_list
 # Delete the second item
-
+mylist.delete_item(2)
 # Print the list
-
+mylist.print_list
 # Update the completion status of the first item to complete
-
+mylist.update_status(1, true)
 # Print the list
-
+mylist.print_list
 # Update the title of the list
-
+mylist.title = "Title is Updated!"
 # Print the list
+mylist.print_list
